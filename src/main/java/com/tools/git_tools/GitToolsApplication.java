5885f1c5ef39b14d2df6d8e78c1f140b5ad6e95c
Marcos Winicios Pereira Martins <mwmartins1@topaz.com.br>
2024-08-10 01:13:45 -0300

    init: Create project

---
+++ src/main/java/com/tools/git_tools/GitToolsApplication.java
@@ -0,0 +1,13 @@
+package com.tools.git_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tools/git_tools/GitToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tools.git_tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
